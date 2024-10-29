fd0d268d9800b90343891c1fd22217333b884093
HUNTER <xujanov.xujamurod@gmail.com>
2024-10-29 19:24:41 +0500

    get resource

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,5 +1,9 @@
 class MarvelService {
-    getResource =  async (url) => {
+    _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+    // ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+    _apiKey = 'apikey=b5fd43db5be36af75c1c848fccda6094';
+
+    getResource = async (url) => {
         let res = await fetch(url);
 
         if (!res.ok) {
@@ -10,7 +14,14 @@ class MarvelService {
     }
 
     getAllCharacters = () => {
-        return this.Resource('https://gateway.marvel.com:443/v1/public/characters?apikey=b5fd43db5be36af75c1c848fccda6094');
+        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
     }
 
+    getCharacter = (id) => {
+        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    }
 }
+
+export default MarvelService;
+
+
