1f2830f6722dfdb6cfde05f967d353f74373cfe9
HUNTER <xujanov.xujamurod@gmail.com>
2024-11-16 18:04:27 +0500

    classwork 16-11-2024 18:04

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,13 +1,57 @@
+import MarvelService from '../../services/MarvelService';
 import { Component } from 'react';
 import './charInfo.scss';
 import thor from '../../resources/img/thor.jpeg';
 
 class CharInfo extends Component {
+
+    state = {
+        char: {},
+        loading: true,
+        error: false
+    }
+
+    marvelService = new MarvelService();
+
+    updateChar = () => {
+        const { charId } = this.props
+        if (!charId) {
+            return
+        }
+
+        this.onCharLoading;
+        this.marvelService
+            .getCharacter(charId)
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+    }
+
+    onCharLoaded = (char) => {
+        this.setState({
+            char, 
+            loading: false
+        })
+    }
+
+    onCharLoading = () => {
+        this.setState({
+            loading: true
+        })
+    }
+
+    onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+
     render() {
         return (
             <div className="char__info">
                 <div className="char__basics">
-                    <img src={thor} alt="abyss"/>
+                    <img src={thor} alt="abyss" />
                     <div>
                         <div className="char__info-name">thor</div>
                         <div className="char__btns">
